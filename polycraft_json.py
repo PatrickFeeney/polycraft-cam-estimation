b11d984abbbb6d6eafc59eab3d499e78d498383a
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-11-01 13:07:52 -0400

    Arg for Ignoring Blocks

--- polycraft_json.py
+++ polycraft_json.py
@@ -12,11 +12,12 @@ def get_p_json_cam(p_json_name):
         return player_pos, yaw, pitch
 
 
-def get_p_json_pts(p_json_name):
+def get_p_json_pts(p_json_name, ignore_blocks=["air"]):
+    ignore_blocks = {"minecraft:" + block for block in ignore_blocks}
     with open(p_json_name, "r") as f:
         p_json = json.load(f)
         pts = np.empty((0, 3))
         for pos, data in p_json["map"].items():
-            if data["name"] != "minecraft:air":
+            if data["name"] not in ignore_blocks:
                 pts = np.concatenate((pts, np.fromstring(pos, sep=",")[np.newaxis]), axis=0)
         return pts.T
