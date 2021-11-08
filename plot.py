2acefbf6cba229544c222aed6e77ed87d82db20d
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-11-08 13:57:53 -0500

    Remove Point Projection Code

--- plot.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-
-def plot_projection(image_name, proj_points):
-    im = plt.imread(image_name)
-    plt.imshow(im)
-    plt.scatter(proj_points[0], proj_points[1], color=[1, 0, 0, .5])
-    plt.show()
